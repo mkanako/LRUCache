b82a0bffbd574e5ab5302670a7abfa99591a381a
kanako <4190346+mkanako@users.noreply.github.com>
2020-07-20 15:05:19 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'standard',
+  ],
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    '@typescript-eslint/no-empty-function': 'off',
+    'comma-dangle': ['error', 'always-multiline'],
+    'quote-props': [
+      'error',
+      'consistent-as-needed',
+    ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+}
