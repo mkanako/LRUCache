fddb40f307234c43accdc31fd6bddccc421a07b6
kanako <4190346+mkanako@users.noreply.github.com>
2020-07-25 23:32:42 +0800

    refactor to handle case when expiry time is 0

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 type KEY_TYPE = string | number
 type NODE_POINTER_TYPE<T> = Node<T> | DummyNode<T>
+type LIST_ITEM_TYPE<T> = [Node<T>, Node<number>] | Node<T>
 
 function time () {
   return (new Date()).getTime()
@@ -61,69 +62,124 @@ class DoublyLinkedList<T> {
   }
 }
 
-export default class LRUCache<T = string> {
-  private map: Map<KEY_TYPE, [Node<T>, Node<number>]>
+class ListContainer<T> {
   private dataList: DoublyLinkedList<T>
-  private timestampList: DoublyLinkedList<number>
+  private timestampList!: DoublyLinkedList<number>
+
+  constructor (private expire: number) {
+    this.dataList = new DoublyLinkedList()
+    if (this.expire > 0) {
+      this.timestampList = new DoublyLinkedList()
+    }
+  }
+
+  size () {
+    return this.dataList.size()
+  }
+
+  create (key: KEY_TYPE, value: T): LIST_ITEM_TYPE<T> {
+    const dataNode = new Node(key, value)
+    if (this.expire > 0) {
+      const timestampNode = new Node(key, time())
+      return [dataNode, timestampNode]
+    }
+    return dataNode
+  }
+
+  getVal (data: LIST_ITEM_TYPE<T>) {
+    if (data instanceof Node) {
+      this.dataList.remove(data)
+      this.dataList.add(data)
+      return data.value
+    } else {
+      if (data[1].value + this.expire >= time()) {
+        this.dataList.remove(data[0])
+        this.dataList.add(data[0])
+        return data[0].value
+      } else {
+        this.dataList.remove(data[0])
+        this.timestampList.remove(data[1])
+      }
+    }
+    return undefined
+  }
+
+  remove (item: LIST_ITEM_TYPE<T>) {
+    if (item instanceof Node) {
+      this.dataList.remove(item)
+    } else {
+      this.dataList.remove(item[0])
+      this.timestampList.remove(item[1])
+    }
+  }
+
+  add (item: LIST_ITEM_TYPE<T>) {
+    if (item instanceof Node) {
+      this.dataList.add(item)
+    } else {
+      this.dataList.add(item[0])
+      this.timestampList.add(item[1])
+    }
+  }
+
+  getClearKey () {
+    if (this.expire > 0) {
+      const tLast = this.timestampList.getLast()
+      if (tLast && time() > tLast.value + this.expire) {
+        return tLast.key
+      }
+    }
+    const dLast = this.dataList.getLast()
+    if (dLast) {
+      return dLast.key
+    }
+  }
+}
+
+export default class LRUCache<T = string> {
+  private map: Map<KEY_TYPE, LIST_ITEM_TYPE<T>>
+  private list: ListContainer<T>
   private capacity: number
   private expire: number
 
-  constructor (capacity = 20, expire = 10 * 60) {
-    this.map = new Map()
-    this.dataList = new DoublyLinkedList()
-    this.timestampList = new DoublyLinkedList()
+  constructor (capacity = 20, expire = 0) {
     this.capacity = capacity
     this.expire = expire * 1000
+    this.map = new Map()
+    this.list = new ListContainer(this.expire)
   }
 
   get (key: KEY_TYPE): T | undefined {
-    const result = this.map.get(key)
-    if (result) {
-      if (result[1].value + this.expire >= time()) {
-        this.dataList.remove(result[0])
-        this.dataList.add(result[0])
-        return result[0].value
-      } else {
-        this.dataList.remove(result[0])
-        this.timestampList.remove(result[1])
+    const item = this.map.get(key)
+    if (item) {
+      const val = this.list.getVal(item)
+      if (val === undefined) {
         this.map.delete(key)
+      } else {
+        return val
       }
     }
     return undefined
   }
 
   set (key: KEY_TYPE, value: T): void {
-    const dataNode = new Node(key, value)
-    const timestampNode = new Node(key, time())
-    const result = this.map.get(key)
-    if (result) {
-      this.dataList.remove(result[0])
-      this.timestampList.remove(result[1])
+    const item = this.list.create(key, value)
+    const oldItem = this.map.get(key)
+    if (oldItem) {
+      this.list.remove(oldItem)
     } else {
-      if (this.dataList.size() >= this.capacity) {
-        const tLast = this.timestampList.getLast()
-        if (tLast && time() > tLast.value + this.expire) {
-          const result = this.map.get(tLast.key)
-          if (result) {
-            this.timestampList.pop()
-            this.dataList.remove(result[0])
-            this.map.delete(tLast.key)
-          }
-        } else {
-          const dLast = this.dataList.getLast()
-          if (dLast) {
-            const result = this.map.get(dLast.key)
-            if (result) {
-              this.dataList.pop()
-              this.timestampList.remove(result[1])
-              this.map.delete(dLast.key)
-            }
+      if (this.list.size() >= this.capacity) {
+        const clearKey = this.list.getClearKey()
+        if (clearKey) {
+          const clearItem = this.map.get(clearKey)
+          if (clearItem) {
+            this.list.remove(clearItem)
+            this.map.delete(clearKey)
           }
         }
       }
     }
-    this.dataList.add(dataNode)
-    this.timestampList.add(timestampNode)
-    this.map.set(key, [dataNode, timestampNode])
+    this.list.add(item)
+    this.map.set(key, item)
   }
 }
